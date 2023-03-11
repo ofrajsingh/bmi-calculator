d5b70a8164848e1c34757331b7f39e4558ac7836
ofrajsingh <rajsingh00seven@gmail.com>
2023-03-12 00:45:21 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Slider from 'react-input-slider'; 
+import { useState } from 'react';
 
 function App() {
+  const [weight, setWeight]= useState(50);
+  const [height, setHeight]= useState(150);
+  const [bmi, setBmi]= useState("22.2");
+
+  const handleWeightChange= e=>{
+    setWeight(e.x);
+    let heightInMetres= height/100;
+    let temp=weight/heightInMetres;
+    temp/=heightInMetres;
+    console.log(temp);
+    setBmi(temp.toFixed(1));
+  }
+  const handleHeightChange= e=>{
+    setHeight(e.x);
+    let heightInMetres= height/100;
+    let temp=weight/heightInMetres;
+    temp/=heightInMetres;
+    console.log(temp);
+    setBmi(temp.toFixed(1));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>RAJ</h1>
+
+      <h1>Weight in Kg: {weight}</h1>
+      <Slider x={weight} onChange={handleWeightChange} axis='x' xmax={200} xmin={1}/>
+
+      <h1>Height in cm: {height}</h1>
+      <Slider x={height} onChange={handleHeightChange} axis='x' xmax={300} xmin={1}/>
+
+
+      <div>
+        <h2>Your BMI is:</h2>
+        <button>{bmi}</button>
+      </div>
+
     </div>
   );
 }
